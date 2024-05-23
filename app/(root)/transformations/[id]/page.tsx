126b6b7e06195d3a9381ceebaa2ed424c4d67760
jerodriguez <jerodriguez@powerstreet.com.mx>
2024-05-23 16:06:31 -0600

    fix id type

--- app/(root)/transformations/[id]/page.tsx
+++ app/(root)/transformations/[id]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TransformationsPage = ({ params: id }) => {
+const TransformationsPage = ({ params: id }: { params: number }) => {
   return (
     <div>TransformationsPage</div>
   )
